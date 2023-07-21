66578aac1b22181e2c4afc83607276025bc54418
McKenzie Jones <mckenzbeck12@gmail.com>
2023-07-21 13:06:28 -0600

    Add function to change from Fahrenheit to Celsius

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const currentTemp = document.getElementById("currentTemp");
 const conditionText = document.getElementById("conditionText");
 const conditionIcon = document.getElementById("conditionIcon");
 const newSearchBtn = document.getElementById("searchBtn");
+const tempTglBtn = document.getElementById("tempTglBtn");
 
 async function getWeather() {
   const response = await fetch(
@@ -15,11 +16,10 @@ async function getWeather() {
   currentLocationName.textContent = weatherData.location.name;
   currentTime.textContent = weatherData.location.localtime;
   currentTemp.textContent = weatherData.current.temp_f + " °" + "F";
+  tempTglBtn.addEventListener("click", tempTgl);
   conditionText.textContent = weatherData.current.condition.text;
   conditionIcon.src = `https://${weatherData.current.condition.icon.substr(2)}`;
 }
-
-getWeather();
 
 const searchNewWeather = () => {
   const searchBar = document.getElementById("searchBar");
@@ -27,6 +27,7 @@ const searchNewWeather = () => {
 };
 
 const fetchNewWeather = async (_) => {
+  removeErrorMessage();
   const searchBar = document.getElementById("searchBar");
   try {
     const response = await fetch(
@@ -50,5 +51,44 @@ const fetchNewWeather = async (_) => {
   }
 };
 
-// Add event listener
+// const getForecastWeather = async () => {
+//   try {
+//     const response = await fetch(
+//       "http://api.weatherapi.com/v1/forecast.json?key=c844ab4c783949409df230416231307&q=London&days=&aqi=no&alerts=no",
+//       { mode: "no-cors" }
+//     );
+//     const weatherForecastData = await response.json();
+//     console.log(weatherForecastData);
+//   } catch (error) {
+//     debugger;
+//   }
+// };
+
+const removeErrorMessage = () => {
+  const errorMessage = document.getElementById("errorMessage");
+  errorMessage.style.display = "none";
+};
+
+const tempTgl = async (_) => {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=c844ab4c783949409df230416231307&q=charleston",
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+
+  if (currentTemp.textContent == weatherData.current.temp_f + " °" + "F") {
+    currentTemp.textContent = weatherData.current.temp_c + " °" + "C";
+    tempTglBtn.value = "°F";
+  } else if (
+    currentTemp.textContent ==
+    weatherData.current.temp_c + " °" + "C"
+  ) {
+    currentTemp.textContent = weatherData.current.temp_f + " °" + "F";
+    tempTglBtn.value = "°C";
+  }
+};
+
+// Add event listener and call functions
+getWeather();
+// getForecastWeather();
 newSearchBtn.addEventListener("click", fetchNewWeather);
