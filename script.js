be98aaf27f10379c223f015e04169aa3bcf39d94
McKenzie Jones <mckenzbeck12@gmail.com>
2023-07-24 20:17:00 -0600

    Add forecast display and CSS

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ const conditionIcon = document.getElementById("conditionIcon");
 const newSearchBtn = document.getElementById("searchBtn");
 const tempTglBtn = document.getElementById("tempTglBtn");
 
-async function getWeather() {
+async function getCurrentWeather() {
   const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=c844ab4c783949409df230416231307&q=charleston",
-    { mode: "cors" }
+    "https://api.weatherapi.com/v1/current.json?key=c844ab4c783949409df230416231307&q=charleston"
   );
   const weatherData = await response.json();
-  console.log(weatherData);
   currentLocationName.textContent = weatherData.location.name;
   currentTime.textContent = weatherData.location.localtime;
   currentTemp.textContent = weatherData.current.temp_f + " °" + "F";
@@ -31,8 +29,7 @@ const fetchNewWeather = async (_) => {
   const searchBar = document.getElementById("searchBar");
   try {
     const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=c844ab4c783949409df230416231307&q=${searchBar.value}`,
-      { mode: "cors" }
+      `https://api.weatherapi.com/v1/current.json?key=c844ab4c783949409df230416231307&q=${searchBar.value}`
     );
     const weatherData = await response.json();
     currentLocationName.textContent = weatherData.location.name;
@@ -47,22 +44,58 @@ const fetchNewWeather = async (_) => {
     errorMessage.style.display = "block";
     errorMessage.textContent =
       "Something went wrong. Please enter a valid city. ";
-    console.log(error);
   }
 };
 
-// const getForecastWeather = async () => {
-//   try {
-//     const response = await fetch(
-//       "http://api.weatherapi.com/v1/forecast.json?key=c844ab4c783949409df230416231307&q=London&days=&aqi=no&alerts=no",
-//       { mode: "no-cors" }
-//     );
-//     const weatherForecastData = await response.json();
-//     console.log(weatherForecastData);
-//   } catch (error) {
-//     debugger;
-//   }
-// };
+const getForecastWeather = async () => {
+  try {
+    // Get forecast data from API
+    const response = await fetch(
+      "http://api.weatherapi.com/v1/forecast.json?key=c844ab4c783949409df230416231307&q=charleston&days=5&aqi=no&alerts=no"
+    );
+    const weatherForecastData = await response.json();
+    console.log(weatherForecastData);
+
+    for (let i = 0; i < weatherForecastData.forecast.forecastday.length; i++) {
+      const forecastArray = weatherForecastData.forecast.forecastday;
+      // Create elements for forecast card and give class names
+      const forecastDisplay = document.getElementById("forecastDisplay");
+      const forecastCard = document.createElement("div");
+      forecastCard.classList.add("forecastCard");
+      const forecastDay = document.createElement("div");
+      forecastDay.classList.add("forecastDay");
+      const forecastIcon = document.createElement("img");
+      forecastIcon.classList.add("forecastIcon");
+      const forecastHighTemp = document.createElement("div");
+      forecastHighTemp.classList.add("forecastHighTemp");
+      const forecastLowTemp = document.createElement("div");
+      forecastLowTemp.classList.add("forecastLowTemp");
+      const forecastPrecipitation = document.createElement("div");
+      forecastPrecipitation.classList.add("forecastPrecipitation");
+
+      // Display day, icon, temps, and precip %
+      let unixTimeStamp = forecastArray[i].date_epoch;
+      let date = new Date(unixTimeStamp * 1000);
+      forecastDay.textContent = date.toString().slice(0, 3);
+      forecastIcon.src = `https://${forecastArray[i].day.condition.icon.substr(
+        2
+      )}`;
+      forecastHighTemp.textContent = `${forecastArray[i].day.maxtemp_f} °F`;
+      forecastLowTemp.textContent = `${forecastArray[i].day.mintemp_f} °F`;
+      forecastPrecipitation.textContent = `${forecastArray[i].day.daily_chance_of_rain}% chance of rain`;
+
+      // Append elements
+      forecastDisplay.append(forecastCard);
+      forecastCard.append(forecastDay);
+      forecastCard.append(forecastIcon);
+      forecastCard.append(forecastHighTemp);
+      forecastCard.append(forecastLowTemp);
+      forecastCard.append(forecastPrecipitation);
+    }
+  } catch (error) {
+    // Something went wrong. Show error
+  }
+};
 
 const removeErrorMessage = () => {
   const errorMessage = document.getElementById("errorMessage");
@@ -89,6 +122,6 @@ const tempTgl = async (_) => {
 };
 
 // Add event listener and call functions
-getWeather();
-// getForecastWeather();
+getCurrentWeather();
+getForecastWeather();
 newSearchBtn.addEventListener("click", fetchNewWeather);
